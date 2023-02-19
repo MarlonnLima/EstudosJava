a0986d51d50a71114350da80c86e6d24c1fee992
MarlonnLima <goncalvesmarlon072@gmail.com>
2023-02-19 16:02:15 -0300

    Variaveis

--- src/TiposVariaveis/TiposVariaveis.java
+++ src/TiposVariaveis/TiposVariaveis.java
@@ -1,4 +1,19 @@
 package TiposVariaveis;
 
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.33;
+        //se um valor começar com zero talvez tenha que ser uma string
+        //tipo float tem que ser colocado F no final, pode ser maiusculo ou minusculo
+        //tipo long tem que terminar com L no final, pode ser maisculo ou minusculo
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //short não pode receber um tipo de dado
+        // maior que ele porque pode ser que em algum momento esse dado seja maior que o limite desse tipo
+        short numeroCurto2 = (short)numeroNormal;
+
+        //para definir que uma variavel é constante use final
+        final double VALOR_DE_PI = 3.14;
+     }
 }
