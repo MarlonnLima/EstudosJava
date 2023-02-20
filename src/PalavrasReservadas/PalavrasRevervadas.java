1bb1510af522db5745b1eb584530b72826e92839
MarlonnLima <goncalvesmarlon072@gmail.com>
2023-02-20 16:17:12 -0300

    métodos

---
+++ src/PalavrasReservadas/PalavrasRevervadas.java
@@ -0,0 +1,9 @@
+package PalavrasReservadas;
+
+public class PalavrasRevervadas {
+    public static void main(String[] args) {
+        //todas as palavras revervadas são minusculas
+        //java possui 52 palavras reservadas
+
+    }
+}
